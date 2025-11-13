fd3c1e84bcb41036b352d0cf96376cfa477bf8ad
loka27e-cmd <loka27e@ncssm.edu>
2025-11-13 14:03:47 -0500

    Added a few variables for BankAccount Class

--- financeforge/src/main/java/com/example/BankAccount.java
+++ financeforge/src/main/java/com/example/BankAccount.java
@@ -1,6 +1,12 @@
 package com.example;
 
 
-public class BankAccount {
-    
+public class BankAccount {    
+    private String userid;
+    private int accountBalance;
+
+    public BankAccount(String userid, int accountBalance) {
+        this.userid = userid;
+        this.accountBalance = accountBalance;
+    }
 }
