4db922b8ffc1cfae3dd1765322c40bee9920a0fa
loka27e-cmd <loka27e@ncssm.edu>
2025-11-13 13:53:03 -0500

    Testing commit

--- financeforge/src/main/java/com/example/Main.java
+++ financeforge/src/main/java/com/example/Main.java
@@ -1,6 +1,6 @@
 package com.example;
 import javafx.application.Application;
-import javafx.scene.Scene;
+import javafx.scene.Scene; // yellow line means we haven't used the module
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
