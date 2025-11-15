c11a8b3fd14301d27984f3c260d517871c49d38b
loka27e-cmd <loka27e@ncssm.edu>
2025-11-15 18:58:51 -0500

    Added two input functions for later use

--- financeforge/src/main/java/com/example/Main.java
+++ financeforge/src/main/java/com/example/Main.java
@@ -5,8 +5,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import java.util.*;
 
 public class Main extends Application {
+
+    static Scanner sc = new Scanner(System.in);
 
     @Override
     public void start(Stage stage) {
@@ -15,5 +18,27 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    // Quickens the input process to just one line
+    public static String getStrInput(String input) {
+    System.out.print("\n");
+    System.out.print(input);
+    
+    return sc.nextLine();
+  }
+
+    // Quickens the input process to just one line (for integer)
+    public static int getIntInput(String input) {
+        while (true){
+        System.out.print("\n");
+        System.out.print(input);
+        
+        try {
+        return Integer.parseInt(sc.nextLine());
+        } catch (NumberFormatException e) {
+        System.out.println("\nThis is not an integer. Pack it up vro");
+        } 
+    }
     }
 }
