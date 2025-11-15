7a3f97b20211b05ece598e0bddab7ac4b46d79b5
loka27e-cmd <loka27e@ncssm.edu>
2025-11-15 18:59:36 -0500

    Added basic methods

--- financeforge/src/main/java/com/example/BankAccount.java
+++ financeforge/src/main/java/com/example/BankAccount.java
@@ -3,10 +3,30 @@ package com.example;
 
 public class BankAccount {    
     private String userid;
-    private int accountBalance;
+    private double accountBalance;
 
-    public BankAccount(String userid, int accountBalance) {
+    public BankAccount(String userid, double accountBalance) {
         this.userid = userid;
         this.accountBalance = accountBalance;
     }
+
+    // All the basic methods associated with this class
+    public double getBalance() {return this.accountBalance;}
+    public String getId() {return this.userid;}
+    public boolean checkId(String inputId) {return inputId.equals(userid);}
+    public void changeId(String id) {this.userid = id;}
+
+    // To Change the Balance of the account
+    public void addToBalance(double money) {this.accountBalance = money;}
+    public void removeFromBalance(double money) {
+        if (money > this.accountBalance) 
+            {this.accountBalance = 0;} 
+        else 
+            {this.accountBalance -= money;}
+    }
+
+    // These two are just in case we end up doing something involving it. We might not use it but I put it here just in case
+    public void multiplyBalance(double multiplier) {this.accountBalance = Math.round(this.accountBalance * multiplier);}
+    public void divideBalance(double divider) {this.accountBalance = Math.round(this.accountBalance / divider);}
+
 }
