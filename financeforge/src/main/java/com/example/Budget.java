5d1adad6a8b5e3b62b5afcf229459efd8b80952b
loka27e-cmd <loka27e@ncssm.edu>
2025-11-15 19:02:41 -0500

    added basic methods

--- financeforge/src/main/java/com/example/Budget.java
+++ financeforge/src/main/java/com/example/Budget.java
@@ -1,5 +1,22 @@
 package com.example;
 
 public class Budget {
+    private double budgetGoal;
+
+    public Budget(double budgetGoal){
+        this.budgetGoal = budgetGoal;
+    }
     
+    public double getBudget() {return this.budgetGoal;}
+
+    // Basically, since we don't have the code for showing information, we will keep it true/false
+    // if it is false, we can let the user know they can't make the new budget (ex. if it is -1, we cant have that as a budget)
+    public boolean changeBudget(double newBudget) {
+        if (newBudget > 0) {
+            this.budgetGoal = newBudget;
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
