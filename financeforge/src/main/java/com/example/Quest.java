b0d171880e01239f17306aef9d1471fcb1fa023b
loka27e-cmd <loka27e@ncssm.edu>
2025-11-17 14:18:44 -0500

    added quest class basics

--- financeforge/src/main/java/com/example/Quest.java
+++ financeforge/src/main/java/com/example/Quest.java
@@ -1,5 +1,11 @@
 package com.example;
 
 public class Quest {
-    
+    private double progress; // in percentage
+
+    public Quest(double progress) {
+        this.progress = progress;
+    }
+
+    public double getProgress() {return this.progress;}
 }
